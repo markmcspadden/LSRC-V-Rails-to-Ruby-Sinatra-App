e3d4566362efc15d9b03c858028dd52c0bd98224
Mark McSpadden <markmcspadden@gmail.com>
2011-08-11 02:59:37 -0500

    Add now_playing_test

--- now_playing.rb
+++ now_playing.rb
@@ -1,16 +1,30 @@
-# myapp.rb
 require 'rubygems'
 require 'sinatra'
 
 require 'sqlite3'
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => File.expand_path(File.dirname(__FILE__) + "/../lsrc-training-center/db/development.sqlite3"))
+db_file_name = if defined?(SINATRA_ENV) && SINATRA_ENV=="test"
+                 "test.sqlite3"
+               else
+                 "development.sqlite3"
+               end
+
+db_file = File.expand_path(File.dirname(__FILE__) + "/../lsrc-training-center/db/#{db_file_name}")
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => db_file)
 
 class ScheduleItem < ActiveRecord::Base
+  @t = self.arel_table
+  
+  scope :now, lambda{ where(@t[:start_at].lteq(Time.now)).where(@t[:end_at].gteq(Time.now)).limit(1) }
 end
 
 get '/now_playing' do
-  item = ScheduleItem.last
- "Now Playing: #{item.title} until #{item.end_at}"
+  item = ScheduleItem.now.first
+  if item
+    "Now Playing: #{item.title} until #{item.end_at}"
+  else
+    "Closed"
+  end
 end
---
+++ now_playing_test.rb
@@ -0,0 +1,41 @@
+SINATRA_ENV="test"
+
+require 'now_playing'
+require 'test/unit'
+require 'rack/test'
+
+class NowPlayingTest < Test::Unit::TestCase
+  include Rack::Test::Methods  
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_active_record_presense
+    assert defined?(ActiveRecord::Base)
+  end
+  
+  def test_active_record_connection
+    assert ActiveRecord::Base.connection
+  end
+
+  def test_now_playing_with_item
+    now = Time.now
+    start_at = now.advance(:minutes => -5)
+    end_at = now.advance(:minutes => 30)
+    
+    ScheduleItem.destroy_all
+    ScheduleItem.create!(:title => "Sinatra Testing", :start_at => start_at, :end_at => end_at)
+    ScheduleItem.create!(:title => "Sinatra Testing", :start_at => nil, :end_at => nil)
+    
+    get '/now_playing'
+    assert_equal "Now Playing: Sinatra Testing until #{end_at}", last_response.body
+  end
+  
+  def test_now_playing_without_item    
+    ScheduleItem.destroy_all
+    
+    get '/now_playing'
+    assert_equal "Closed", last_response.body
+  end
+end
