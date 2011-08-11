f0333a6405e84c2acdf98eb7198f27cf77a5bfaa
Mark McSpadden <markmcspadden@gmail.com>
2011-08-11 03:04:55 -0500

    Strip away working now_playing

--- now_playing.rb
+++ now_playing.rb
@@ -1,9 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 
-require 'sqlite3'
-require 'active_record'
-
 db_file_name = if defined?(SINATRA_ENV) && SINATRA_ENV=="test"
                  "test.sqlite3"
                else
@@ -12,19 +9,9 @@ db_file_name = if defined?(SINATRA_ENV) && SINATRA_ENV=="test"
 
 db_file = File.expand_path(File.dirname(__FILE__) + "/../lsrc-training-center/db/#{db_file_name}")
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => db_file)
-
-class ScheduleItem < ActiveRecord::Base
-  @t = self.arel_table
-  
-  scope :now, lambda{ where(@t[:start_at].lteq(Time.now)).where(@t[:end_at].gteq(Time.now)).limit(1) }
+class ScheduleItem
 end
 
-get '/now_playing' do
-  item = ScheduleItem.now.first
-  if item
-    "Now Playing: #{item.title} until #{item.end_at}"
-  else
-    "Closed"
-  end
+get '/' do
+  "Hello World!"
 end
